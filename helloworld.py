484734e6a5d91cc7745830608cc0d5d72ddd9b75
Owen Wells <owenwells@Owens-MacBook-Pro.local>
2025-07-06 23:09:31 -0700

    added median

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,11 @@
-print('hello, world!')
+import math
+
+def median(input):
+    size = len(input)
+    if size % 2 == 0:
+        value1 = size // 2 - 1
+        value2 = size // 2
+        return (input[value1] + input[value2]) / 2
+    else:
+        _value = size // 2
+        return input[_value]
